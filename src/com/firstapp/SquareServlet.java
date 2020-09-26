06a78754be8abdaf2b123ab75a07bf5e0a6b5251
Gizmosoft <kartikey.hebbar@gmail.com>
2020-09-26 13:27:32 +0530

    Add & Sq servlets

---
+++ src/com/firstapp/SquareServlet.java
@@ -0,0 +1,25 @@
+package com.firstapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SquareServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+		//below  for RequestDispatcher		
+//		int k = (int) req.getAttribute("k");
+//		k = k*k;
+		
+		// below for sendRedirect
+		int k = Integer.parseInt(req.getParameter("k"));
+		k = k*k;
+		
+		// below for Printing Here using this Servlet
+		PrintWriter out = res.getWriter();
+		out.println("result is: "+ k);
+	}
+}
